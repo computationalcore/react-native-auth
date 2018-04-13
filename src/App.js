baac2bf9cef4a063a5bd5b8fe49c490fd9ddb4ae
Vin Busquet <computationalcore@gmail.com>
2018-04-13 12:39:52 -0300

    feat: Add base react generated files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.welcome}>An App!</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
