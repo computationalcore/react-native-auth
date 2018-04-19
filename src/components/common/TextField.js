0c4ad30ff92cac250d57db653442a21baa539679
Vin Busquet <computationalcore@gmail.com>
2018-04-18 21:05:10 -0300

    feat: Extend TextField component

--- src/components/common/TextField.js
+++ src/components/common/TextField.js
@@ -16,6 +16,9 @@ const TextField = (props) => {
     <View style={containerStyle}>
       <Text style={labelStyle}>{props.label}</Text>
       <TextInput
+        secureTextEntry={props.secureTextEntry}
+        placeholder={props.placeholder}
+        autoCorrect={false}
         style={inputStyle}
         value={props.value}
         onhangeText={props.onChangeText}
