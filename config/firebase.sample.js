b7438eb2c691a69a312d64d2d5364fb998fd85ad
Vin Busquet <computationalcore@gmail.com>
2018-05-15 12:36:03 -0300

    refactor: Change firebase config logic

--- config/firebase.sample.js
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_SETUP = {
-  apiKey: 'YOUR-API-KEY',
-  authDomain: 'YOUR-AUTH-DOMAIN',
-  databaseURL: 'YOUR-DATABASE-URL',
-  projectId: 'YOUR-PROJECT-ID',
-  storageBucket: 'YOUR-STORAGE-BUCKET',
-  messagingSenderId: 'YOUR-SENDER_ID'
-};
